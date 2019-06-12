97c2b05dc32a483926d94badaec433dccef8453b
Paul Han <kyutae.han93@gmail.com>
2019-06-12 11:12:33 -0400

    added createItem method on Api module

--- scripts/api.js
+++ scripts/api.js
@@ -3,13 +3,27 @@
 // eslint-disable-next-line no-unused-vars
 
 const Api = (function(){
-  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/NPaul5656';
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/NPaul5656/';
   const getItems = function(){
-    return Promise.resolve('A successful response!');
+    return fetch(`${BASE_URL}items`);
+  };
+
+  const createItem = function(name){
+    const newItem= JSON.stringify({
+      name,
+    });
+    const options={
+      method:'POST',
+      body: newItem,
+      headers: new Headers({'Content-Type': 'application/json'})
+    };
+    return fetch(`${BASE_URL}items`, options);
+
   };
 
 
   return {
     getItems,
+    createItem,
   };
 }());
