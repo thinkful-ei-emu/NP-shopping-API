e385a514ca526f5d435074440ffb8ee11502c1ae
Nicholas Gunter <ygnick@live.com>
2019-06-12 11:32:35 -0400

    updated handleNewItemsSubmit to use Api

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,13 @@
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  Api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
 });
 
-store.items.push(Item.create('apples'));
+
 
